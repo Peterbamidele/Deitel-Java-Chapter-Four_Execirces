1bbfbdc7bcb2daaeb2f59ddfca54b40d255529c0
Peterbamidele <peterbamidele@gmail.com>
2020-12-18 05:37:36 +0100

    initial ommit

---
+++ Counter.java
@@ -0,0 +1,10 @@
+public class Counter {
+    public static void main(System[] args){
+                for (int i = 0; i < 10; i++) {
+                    if (i == 4) {
+                        continue;
+                    }
+                    System.out.println(i);
+                }
+            }
+        }
